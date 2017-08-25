65dbdbcfef9dc5618472313bf6bdf27585a98bfe
Tomasz Żok <tomasz.zok@cs.put.poznan.pl>
2017-08-25 16:46:44 +0200

    Finish development work on Kepler Status Portlet

--- kepler-status-portlet/src/main/java/pl/psnc/indigo/kepler/status/portlet/KeplerPortlet.java
+++ kepler-status-portlet/src/main/java/pl/psnc/indigo/kepler/status/portlet/KeplerPortlet.java
@@ -1,16 +1,20 @@
 package pl.psnc.indigo.kepler.status.portlet;
 
+import aQute.bnd.annotation.metatype.Configurable;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.security.sso.iam.model.TokenInfo;
 import com.liferay.portal.security.sso.iam.service.TokenServiceUtil;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.psnc.indigo.fg.api.restful.TasksAPI;
 import pl.psnc.indigo.fg.api.restful.exceptions.FutureGatewayException;
+import pl.psnc.indigo.fg.api.restful.jaxb.Task;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
@@ -19,10 +23,21 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 // @formatter:off
 @Component(
         immediate = true,
+        configurationPid = "pl.psnc.indigo.kepler.status.portlet.KeplerPortletConfiguration",
         property = {
             "com.liferay.portlet.css-class-wrapper=portlet-greeter",
             "com.liferay.portlet.display-category=category.sample",
@@ -36,7 +51,10 @@ import java.net.URI;
         service = Portlet.class)
 // @formatter:on
 public class KeplerPortlet extends MVCPortlet {
-    private static final Log LOG = LogFactoryUtil.getLog(KeplerPortlet.class);
+    private static final Logger LOG =
+            LoggerFactory.getLogger(KeplerPortlet.class);
+
+    private volatile KeplerPortletConfiguration configuration;
 
     @Override
     public final void doView(final RenderRequest renderRequest,
@@ -45,19 +63,26 @@ public class KeplerPortlet extends MVCPortlet {
         try {
             final TokenInfo tokenInfo =
                     KeplerPortlet.getTokenInfo(renderRequest);
-
-            if ((tokenInfo != null) && (tokenInfo.getSubject() != null)) {
-                renderRequest.setAttribute("isIamUser", true);
+            if ((tokenInfo == null) || (tokenInfo.getSubject() == null)) {
                 renderRequest
-                        .setAttribute("iamSubject", tokenInfo.getSubject());
-
-                final TasksAPI api = new TasksAPI(
-                        URI.create("http://fgw02.ncg.ingrid.pt/apis"),
-                        tokenInfo.getToken());
-            } else {
-                renderRequest.setAttribute("isIamUser", false);
+                        .setAttribute("error", "You must log in using IAM");
+                return;
             }
-        } catch (final PortalException | FutureGatewayException e) {
+
+            final URI futureGatewayUri = configuration.futureGatewayUri();
+            if (futureGatewayUri == null) {
+                renderRequest.setAttribute("error",
+                                           "You must configure FutureGateway " +
+                                           "URI");
+                return;
+            }
+
+            final Collection<Task> tasks =
+                    KeplerPortlet.getTasks(tokenInfo, futureGatewayUri);
+
+            renderRequest.setAttribute("tasks", tasks);
+        } catch (final PortalException | FutureGatewayException |
+                InterruptedException | ExecutionException e) {
             throw new PortletException(
                     "Failed to prepare variables for Kepler portlet", e);
         } finally {
@@ -66,10 +91,50 @@ public class KeplerPortlet extends MVCPortlet {
         }
     }
 
+    private static Collection<Task> getTasks(final TokenInfo tokenInfo,
+                                             final URI futureGatewayUri)
+            throws FutureGatewayException, InterruptedException,
+                   ExecutionException {
+        final TasksAPI api =
+                new TasksAPI(futureGatewayUri, tokenInfo.getToken());
+
+        final int cpuCount = Runtime.getRuntime().availableProcessors();
+        final ExecutorService executorService =
+                Executors.newFixedThreadPool(2 * cpuCount);
+        final Collection<Future<Collection<Task>>> futures = new ArrayList<>();
+
+        for (final Task task : api.getAllTasks()) {
+            futures.add(executorService.submit(() -> {
+                final Task taskFull = api.getTask(task.getId());
+                if (!taskFull.getRuntimeData().isEmpty()) {
+                    return Collections.singletonList(taskFull);
+                }
+                return Collections.emptyList();
+            }));
+        }
+
+        executorService.shutdown();
+
+        final Collection<Task> tasks = new TreeSet<>(
+                Comparator.comparing(Task::getLastChange).reversed());
+        for (final Future<Collection<Task>> future : futures) {
+            tasks.addAll(future.get());
+        }
+        return tasks;
+    }
+
     private static TokenInfo getTokenInfo(final PortletRequest renderRequest)
             throws PortalException {
         final ServiceContext serviceContext =
                 ServiceContextFactory.getInstance(renderRequest);
         return TokenServiceUtil.getToken(serviceContext);
+    }
+
+    @Activate
+    @Modified
+    protected final void activate(final Map<String, Object> properties) {
+        configuration = Configurable
+                .createConfigurable(KeplerPortletConfiguration.class,
+                                    properties);
     }
 }
--- kepler-status-portlet/src/main/resources/META-INF/resources/view.jsp
+++ kepler-status-portlet/src/main/resources/META-INF/resources/view.jsp
@@ -1,18 +1,87 @@
+<%--suppress LongLine --%>
 <%@ include file="init.jsp" %>
 
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page import="pl.psnc.indigo.fg.api.restful.jaxb.Task" %>
+<%@ page import="pl.psnc.indigo.fg.api.restful.jaxb.TaskStatus" %>
+<%@ page import="java.util.Collection" %>
 
 <c:choose>
 
-    <c:when test="${renderRequest.getAttribute('isIamUser')}">
+    <c:when test="${renderRequest.getAttribute('error') == null}">
 
-        Hello <c:out value="${renderRequest.getAttribute('iamSubject')}"/>
+        <%--@elvariable id="tasks" type="java.util.Collection<Task>"--%>
+
+        <%
+
+            final Collection<Task> tasks = (Collection<Task>) renderRequest.getAttribute("tasks");
+
+        %>
+
+        You have <c:out value="${tasks.size()}"/> tasks with runtime data.
+
+        <div class="panel-group" id="accordion" role="tablist">
+
+            <c:forEach var="task" items="${tasks}">
+
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="heading<c:out value="${task.id}"/>">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#task<c:out value="${task.id}"/>" aria-controls="task<c:out value="${task.id}"/>"><span class="glyphicon
+                            <c:choose>
+                                <c:when test="${task.status == 'DONE'}">
+                                    glyphicon-ok
+                                </c:when>
+                                <c:when test="${task.status == 'ABORTED'}">
+                                    glyphicon-remove
+                                </c:when>
+                                <c:otherwise>
+                                    glyphicon-refresh
+                                </c:otherwise>
+                            </c:choose>"></span> ${task.lastChange} - <c:out value="${task.id}"/> - <c:out value="${task.status}"/><c:if test="${task.description.length() > 0}">
+                                <br/>
+                                <c:out value="${task.description}"/></c:if> </a>
+                        </h4>
+                    </div>
+
+                    <div id="task<c:out value="${task.id}"/>" class="collapse panel-collapse" role="tabpanel" aria-labelledby="header<c:out value="${task.id}"/>">
+                        <div class="panel-body">
+                            <table class="table table-striped" style="width: 100%; table-layout: fixed">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Created</th>
+                                    <th>Last changed</th>
+                                    <th>Value</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                <c:forEach var="runtimeData" items="${task.runtimeData}">
+
+                                    <tr>
+                                        <td><c:out value="${runtimeData.name}"/></td>
+                                        <td><c:out value="${runtimeData.creation}"/></td>
+                                        <td><c:out value="${runtimeData.lastChange}"/></td>
+                                        <td style="max-width: 25%;"><span style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis; display: inline-block; max-width: 100%;"><c:out value="${runtimeData.value}"/></span></td>
+                                    </tr>
+
+                                </c:forEach>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </c:forEach>
+
+        </div>
 
     </c:when>
 
     <c:otherwise>
 
-        You must be logged in using IAM.
+        <c:out value="${renderRequest.getAttribute('error')}"/>
 
     </c:otherwise>
 
